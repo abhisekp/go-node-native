61243ac2c2bdc5f958ab1e6e4e049d84df9c0ea3
Abhisek Pattnaik <abhisekp@engineer.com>
2025-02-15 02:44:55 -0500

    fix: sum arguments in usage

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import { myHandler, sum } from "@abhisekp/go-native";
 (function main() {
   console.log("Start");
   console.log("Output:", myHandler());
-  console.log("Sum of 1 and 2:", sum(1, "2"));
+  console.log("Sum of 1 and 2:", sum(1, 2));
   console.log("End");
 })();
